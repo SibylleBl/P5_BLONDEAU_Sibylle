fdbf14b54a0604c47f234069aaf41113c506d619
Sibylle Blondeau <sibylle.blondeau@hotmail.fr>
2022-06-02 17:51:59 +0200

    création fonction get pour local storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,35 @@
-getItemsLocalStorage();
+// let produitTableau = JSON.parse(localStorage.getItem("produit")); // qu'ai-je dans mon panier?
+// console.log("🚀 ~ file: cart.js ~ line 2 ~ produitTableau", produitTableau);
+
+// if (produitTableau == null) {
+//   // si mon produit tableau est null, définir produitTableau en tableau
+//   produitTableau = []; //création du tableau
+//   localStorage.setItem("produit", JSON.stringify(produitTableau)); // j'envoie un produit sous forme de string
+// }
+
+//TUTO TEST pour comprendre
+// function savebasket(basket) {
+//   localStorage.setItem("monPanier", JSON.stringify(basket));
+// }
+
+// function getBasket() {
+//   let basket = localStorage.getItem("basket");
+//   if (basket == null) {
+//     return [];
+//   } else {
+//     return JSON.parse(basket);
+//   }
+// }
+
+// function addBasket(product) {
+//   let basket = getBasket();
+//   let foundProduct = basket.find((p) => p.id == product.id);
+//   if (foundProduct != undefined) {
+//     foundProduct.quantity++;
+//   } else {
+//     product.quantity = 1; //quantité minimum par défaut
+//     basket.push(product);
+//   }
+//   basket.push(product);
+//   savebasket(basket);
+// }
--- front/js/product.js
+++ front/js/product.js
@@ -1,23 +1,23 @@
 pagearticle = new URL(window.location.href);
-console.log(pagearticle);
 id_article = new URL(window.location.href).searchParams.get("id");
-console.log(id_article);
 let url = new URL("http://localhost:3000/api/products.html?id=" + id_article);
 
-// const products= []
+function getItemsFromLocalStorage() {
+  let produitTableau = localStorage.getItem("produit"); // m'affiche le contenu du panier
+  console.log(produitTableau);
+}
+
 fetch(`http://localhost:3000/api/products/${id_article}`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     const canape = data;
 
     const canapePicture = document.getElementsByClassName("item__img");
     `<img src="${canape.imageUrl}" alt="Photographie d'un canapé"></img>`;
     canapePicture[0].innerHTML = `<img src="${canape.imageUrl}" alt="${canape.altTxt}"></img>`;
-
-    //L'image ne s'affiche pas (ReferenceError: imageUrl is not defined at product.js:22:63)
 
     const laoujinjectemonhtml = document.getElementById("title");
     laoujinjectemonhtml.innerHTML = canape.name;
@@ -32,10 +32,6 @@ fetch(`http://localhost:3000/api/products/${id_article}`)
     let optionsdecouleurs = "";
 
     canape.colors.forEach((color) => {
-      console.log(
-        "🚀 ~ file: product.js ~ line 25 ~ canape.colors.forEach ~ color",
-        color
-      );
       optionsdecouleurs += `<option value="${color}">${color}</option>`;
     });
 
@@ -43,32 +39,45 @@ fetch(`http://localhost:3000/api/products/${id_article}`)
 
     // ajout d'un event lorque je clique sur le bouton "ajouter au panier" :
     clickButton = document.getElementById("addToCart");
-    console.log("🚀 ~ file: product.js ~ line 54 ~ clickButton", clickButton);
-
-    // clickButton.addEventListener("click", choixCouleur);
-    // clickButton.addEventListener("click", choixQuantite);
+    // console.log("🚀 ~ file: product.js ~ line 54 ~ clickButton", clickButton);
 
     //Récuperer la valeur choisie dans le sélecteur des couleurs:
     function choixCouleur() {
       selectColor = document.getElementById("colors");
       value = selectColor.value;
-      console.log(value);
+      // console.log(value);
       return value;
     }
 
     //Récuperer la quantité choisie dans l'input du nombre d'article:
     function choixQuantite() {
       selectQuant = document.getElementById("quantity").value;
-      console.log(selectQuant);
+      // console.log(selectQuant);
       return selectQuant;
     }
+
+    // je regroupe les différents choix du client au même endroit:
     function clickChoixDuClient() {
       const choixDuClient = {
-        id: id_article,
-        couleur: choixCouleur(),
-        quantite: choixQuantite(),
+        id: id_article, //ref du produit
+        couleur: choixCouleur(), // couleur choisie
+        quantite: choixQuantite(), // quantité choisie
       };
-      console.log(choixDuClient);
+      // console.log(choixDuClient);
     }
+
+    function setItemsToLocalStorage() {
+      const monObjetQueJeConvertisEnString = JSON.stringify(clickChoixDuClient);
+      console.log(monObjetQueJeConvertisEnString);
+      let envoiProduit = localStorage.setItem("produit", clickChoixDuClient);
+      console.log(envoiProduit);
+    }
+
     clickButton.addEventListener("click", clickChoixDuClient);
+
+    //j'appelle la fonction pour afficher le contenu du panier
+    getItemsFromLocalStorage();
+
+    // j'appelle la fonction pour envoyer un article dans le panier
+    setItemsToLocalStorage();
   });
